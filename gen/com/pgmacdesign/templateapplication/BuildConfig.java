71da7f06e4ebf61af5082b4b70ee122ec5ec9098
PGMacDesign <pgmacdesign@gmail.com>
2014-07-16 01:19:35 -0700

    This is a template for standard app setup

---
+++ gen/com/pgmacdesign/templateapplication/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pgmacdesign.templateapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
